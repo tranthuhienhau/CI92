ed06264304eae2f0e3f1e281f480d18a527c7870
tranthuhienhau <thuhientran170102@gmail.com>
2024-01-30 11:47:24 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,39 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-// import React, {useState} from 'react';
+import React, {useState} from 'react';
 
+const App = () => {
+  const[value, setValue] = useState("");
+  const[todolist, setTodolist]=useState([]);
+  const addTodo = ()=>{
+    if(!value)
+    {
+      alert("You shoudn't be empty");
+      return;
 
-// import Button from './component/Button'
-// const ParentComponent = () => {
-//   return (
-//     <div>
-//       <div className="btn-top">
-//       <Button text='Button 1'  />
-//       <Button text='Button 2'/>
-//       <Button text='Button 3' />
-//     </div>
-//     <div className='Container'>
-//       <div className="btn-left">
-//       <Button text='Button 1'  />
-//       <Button text='Button 2'/>
-//       <Button text='Button 3' />
-//       <Button text='Button 4'/>
-//       </div>
-//       <div className="btn-right">
-//       <Button text='Button 1'  />
-//       <Button text='Button 2'/>
-//       <Button text='Button 3' />
-//       </div>
-//     </div>
-//      </div>
-   
-        
-//       );
-//     };
-//     export default ParentComponent
+    }
+    setTodolist([...todolist,value])
+      alert("Add is success")
+      setValue("")
     
-  
+
+  }
+  return (
+    <div>
+      <h1>Tinder</h1>
+      <input type="text" value={value} onChange={(e)=> setValue(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
+      <div>
+        {
+          todolist.map((todo)=>(
+            <h3>{todo}</h3>
+          ))
+        }
+      </div>
+      </div>
+    
+  )
+}
+
+export default App
