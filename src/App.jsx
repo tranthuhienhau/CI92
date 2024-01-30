d23d1c60cf9f4164ee689162a91b819caae8dd68
tranthuhienhau <thuhientran170102@gmail.com>
2024-01-30 12:09:00 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,95 @@
 // import React from 'react';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css'
 import React, {useState} from 'react';
 
 const App = () => {
-  const[value, setValue] = useState("");
-  const[todolist, setTodolist]=useState([]);
-  const addTodo = ()=>{
-    if(!value)
-    {
-      alert("You shoudn't be empty");
+  const [form, setFormData] = useState({
+    fullName: '',
+    emailAddress: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const handleChange = (e) => {
+    setFormData({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      form.fullName.trim() === '' ||
+      form.emailAddress.trim() === '' ||
+      form.password.trim() === '' ||
+      form.confirmPassword.trim() === ''
+    ) {
+      alert('You must fill in all fields');
       return;
-
     }
-    setTodolist([...todolist,value])
-      alert("Add is success")
-      setValue("")
-    
 
-  }
+
+    console.log('Form submitted:', form);
+    alert(JSON.stringify(form));
+    setFormData({
+      fullName: '',
+      emailAddress: '',
+      password: '',
+      confirmPassword: '',
+    });
+  };
+
   return (
-    <div>
-      <h1>Tinder</h1>
-      <input type="text" value={value} onChange={(e)=> setValue(e.target.value)} />
-      <button onClick={addTodo}>Add</button>
-      <div>
-        {
-          todolist.map((todo)=>(
-            <h3>{todo}</h3>
-          ))
-        }
-      </div>
-      </div>
-    
-  )
-}
+    <form action="" id="form_hello" onSubmit={handleSubmit}>
+      <h3>HELLO</h3>
+      <h4>Please sign up to continue</h4>
+
+      <label htmlFor="fullName">Fullname</label>
+      <input
+        type="text"
+        id="fullName"
+        name="fullName"
+        value={form.fullName}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="emailAddress">Email address</label>
+      <input
+        type="text"
+        id="emailAddress"
+        name="emailAddress"
+        value={form.emailAddress}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={form.password}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="confirmPassword">Confirm Password</label>
+      <input
+        type="password"
+        id="confirmPassword"
+        name="confirmPassword"
+        value={form.confirmPassword}
+        onChange={handleChange}
+        required
+      />
+
+      <button type="submit">Sign up</button>
+    </form>
+  );
+};
+
 
 export default App
